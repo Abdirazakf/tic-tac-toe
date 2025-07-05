eba67fb9e320bc0e88b3d32266223733612a96db
Abdirazakf <farahabdirazak13@gmail.com>
2025-07-05 19:45:54 -0400

    Fixed turn system bug and added some basic styling for the page

--- scripts.js
+++ scripts.js
@@ -19,15 +19,15 @@
     
     const UI = (() => {
         const elements = {
-            slot1: document.querySelector(".slot-1"),
-            slot2: document.querySelector(".slot-2"),
-            slot3: document.querySelector(".slot-3"),
-            slot4: document.querySelector(".slot-4"),
-            slot5: document.querySelector(".slot-5"),
-            slot6: document.querySelector(".slot-6"),
-            slot7: document.querySelector(".slot-7"),
-            slot8: document.querySelector(".slot-8"),
-            slot9: document.querySelector(".slot-9")
+            slot1: document.querySelector("#slot-1"),
+            slot2: document.querySelector("#slot-2"),
+            slot3: document.querySelector("#slot-3"),
+            slot4: document.querySelector("#slot-4"),
+            slot5: document.querySelector("#slot-5"),
+            slot6: document.querySelector("#slot-6"),
+            slot7: document.querySelector("#slot-7"),
+            slot8: document.querySelector("#slot-8"),
+            slot9: document.querySelector("#slot-9")
         }
         
         const getElement = () => elements
@@ -42,13 +42,15 @@
         let elements = UI.getElement()
         
         const slot1Logic = () => {
-            if ((gameboard.moveCounter % 2 == 1) && board[0] !== "O" && !gameOver){
+            if ((gameboard.moveCounter % 2 == 1)  && board[0] !== "X" && board[0] !== "O" && !gameOver){
                 board[0] = "X"
                 gameboard.moveCounter++
+                console.log(gameboard.moveCounter)
                 elements.slot1.textContent = "X"
-            } else if ((gameboard.moveCounter % 2 == 0) && board[0] !== "X" && !gameOver){
+            } else if ((gameboard.moveCounter % 2 == 0)  && board[0] !== "O" && board[0] !== "X" && !gameOver){
                 board[0] = "O"
                 gameboard.moveCounter++
+                console.log(gameboard.moveCounter)
                 elements.slot1.textContent = "O"
             }
 
@@ -58,13 +60,15 @@
         }
         
         const slot2Logic = () => {
-            if ((gameboard.moveCounter % 2 == 1 ) && board[1] !== "O" && !gameOver){
+            if ((gameboard.moveCounter % 2 == 1)  && board[1] !== "X" && board[1] !== "O" && !gameOver){
                 board[1] = "X"
                 gameboard.moveCounter++
+                console.log(gameboard.moveCounter)
                 elements.slot2.textContent = "X"
-            } else if ((gameboard.moveCounter % 2 == 0) && board[1] !== "X" && !gameOver){
+            } else if ((gameboard.moveCounter % 2 == 0)  && board[1] !== "O" && board[1] !== "X" && !gameOver){
                 board[1] = "O"
                 gameboard.moveCounter++
+                console.log(gameboard.moveCounter)
                 elements.slot2.textContent = "O"
             }
             
@@ -74,13 +78,15 @@
         }
         
         const slot3Logic = () => {
-            if ((gameboard.moveCounter % 2 == 1 ) && board[2] !== "O" && !gameOver){
+            if ((gameboard.moveCounter % 2 == 1)  && board[2] !== "X" && board[2] !== "O" && !gameOver){
                 board[2] = "X"
                 gameboard.moveCounter++
+                console.log(gameboard.moveCounter)
                 elements.slot3.textContent = "X"
-            } else if ((gameboard.moveCounter % 2 == 0) && board[2] !== "X" && !gameOver){
+            } else if ((gameboard.moveCounter % 2 == 0)  && board[2] !== "O" && board[2] !== "X" && !gameOver){
                 board[2] = "O"
                 gameboard.moveCounter++
+                console.log(gameboard.moveCounter)
                 elements.slot3.textContent = "O"
             }
             
@@ -90,13 +96,15 @@
         }
 
         const slot4Logic = () => {
-            if ((gameboard.moveCounter % 2 == 1 ) && board[3] !== "O" && !gameOver){
+            if ((gameboard.moveCounter % 2 == 1)  && board[3] !== "X" && board[3] !== "O" && !gameOver){
                 board[3] = "X"
                 gameboard.moveCounter++
+                console.log(gameboard.moveCounter)
                 elements.slot4.textContent = "X"
-            } else if ((gameboard.moveCounter % 2 == 0) && board[3] !== "X" && !gameOver){
+            } else if ((gameboard.moveCounter % 2 == 0)  && board[3] !== "O" && board[3] !== "X" && !gameOver){
                 board[3] = "O"
                 gameboard.moveCounter++
+                console.log(gameboard.moveCounter)
                 elements.slot4.textContent = "O"
             }
 
@@ -106,13 +114,15 @@
         }
             
         const slot5Logic = () => {
-            if ((gameboard.moveCounter % 2 == 1 ) && board[4] !== "O" && !gameOver){
+            if ((gameboard.moveCounter % 2 == 1)  && board[4] !== "X" && board[4] !== "O" && !gameOver){
                 board[4] = "X"
                 gameboard.moveCounter++
+                console.log(gameboard.moveCounter)
                 elements.slot5.textContent = "X"
-            } else if ((gameboard.moveCounter % 2 == 0) && board[4] !== "X" && !gameOver){
+            } else if ((gameboard.moveCounter % 2 == 0)  && board[4] !== "O" && board[4] !== "X" && !gameOver){
                 board[4] = "O"
                 gameboard.moveCounter++
+                console.log(gameboard.moveCounter)
                 elements.slot5.textContent = "O"
             }
             
@@ -122,13 +132,15 @@
         }
 
         const slot6Logic = () => {
-            if ((gameboard.moveCounter % 2 == 1 ) && board[5] !== "O" && !gameOver){
+            if ((gameboard.moveCounter % 2 == 1)  && board[5] !== "X" && board[5] !== "O" && !gameOver){
                 board[5] = "X"
                 gameboard.moveCounter++
+                console.log(gameboard.moveCounter)
                 elements.slot6.textContent = "X"
-            } else if ((gameboard.moveCounter % 2 == 0) && board[5] !== "X" && !gameOver){
+            } else if ((gameboard.moveCounter % 2 == 0)  && board[5] !== "O" && board[5] !== "X" && !gameOver){
                 board[5] = "O"
                 gameboard.moveCounter++
+                console.log(gameboard.moveCounter)
                 elements.slot6.textContent = "O"
             }
             
@@ -138,13 +150,15 @@
         }
         
         const slot7Logic = () => {
-            if ((gameboard.moveCounter % 2 == 1 ) && board[6] !== "O" && !gameOver){
+            if ((gameboard.moveCounter % 2 == 1)  && board[6] !== "X" && board[6] !== "O" && !gameOver){
                 board[6] = "X"
                 gameboard.moveCounter++
+                console.log(gameboard.moveCounter)
                 elements.slot7.textContent = "X"
-            } else if ((gameboard.moveCounter % 2 == 0) && board[6] !== "X" && !gameOver){
+            } else if ((gameboard.moveCounter % 2 == 0)  && board[6] !== "O" && board[6] !== "X" && !gameOver){
                 board[6] = "O"
                 gameboard.moveCounter++
+                console.log(gameboard.moveCounter)
                 elements.slot7.textContent = "O"
             }
             
@@ -154,13 +168,15 @@
         }
       
         const slot8Logic = () => {
-            if ((gameboard.moveCounter % 2 == 1 ) && board[7] !== "O" && !gameOver){
+            if ((gameboard.moveCounter % 2 == 1)  && board[7] !== "X" && board[7] !== "O" && !gameOver){
                 board[7] = "X"
                 gameboard.moveCounter++
+                console.log(gameboard.moveCounter)
                 elements.slot8.textContent = "X"
-            } else if ((gameboard.moveCounter % 2 == 0) && board[7] !== "X" && !gameOver){
+            } else if ((gameboard.moveCounter % 2 == 0)  && board[7] !== "O" && board[7] !== "X" && !gameOver){
                 board[7] = "O"
                 gameboard.moveCounter++
+                console.log(gameboard.moveCounter)
                 elements.slot8.textContent = "O"
             }
             
@@ -170,13 +186,15 @@
         }
 
         const slot9Logic = () => {
-            if ((gameboard.moveCounter % 2 == 1 ) && board[8] !== "O" && !gameOver){
+            if ((gameboard.moveCounter % 2 == 1)  && board[8] !== "X" && board[8] !== "O" && !gameOver){
                 board[8] = "X"
                 gameboard.moveCounter++
+                console.log(gameboard.moveCounter)
                 elements.slot9.textContent = "X"
-            } else if ((gameboard.moveCounter % 2 == 0) && board[8] !== "X" && !gameOver){
+            } else if ((gameboard.moveCounter % 2 == 0)  && board[8] !== "O" && board[8] !== "X" && !gameOver){
                 board[8] = "O"
                 gameboard.moveCounter++
+                console.log(gameboard.moveCounter)
                 elements.slot9.textContent = "O"
             }
             
@@ -280,8 +298,7 @@
                         count++ 
                     }
                 }
-                console.log(count)
-                
+
                 if (count >= 9){
                     alert("It's a Draw")
                     location.reload()
